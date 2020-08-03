248a2e076e80e9379407b393f6defa589a0c014a
J White <jmw-81@hotmail.co.uk>
2020-08-03 18:46:39 +0000

    edit to js function

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    //original code was:
+    //$("#stream1_btn").on("click", function()
+    $("#stream1_btn").mouseenter(function() {
+   
+    //alternatives include:
+    //$("#stream1_btn").click(function()
+    //$("#stream1_btn").mouseenter(function()
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+   });
+   $("#stream2_btn").mouseenter(function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").mouseenter(function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+}); 
